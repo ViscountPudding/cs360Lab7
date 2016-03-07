e9042e9646b95b86cceac85ec504118ab04982b8
ViscountPudding <stephenmccammon@comcast.net>
2016-03-07 02:27:26 +0000

    whee

--- nodeSeven/public/nodeServer.js
+++ nodeSeven/public/nodeServer.js
@@ -1,11 +1,13 @@
 var fs = require('fs')
 var http = require('http')
 var url = require('url')
-var ROOT_DIR = "html/"
+var ROOT_DIR = "/"
+
 
 http.createServer(function (req, res) {
 
-	var urlObj = url.parse(req.url, true, false)
+	var urlObj = url.parse(req.url, true, false) + "cityfinder.html"
+	console.log(urlObj);
 	fs.readFile(ROOT_DIR + urlObj.pathname, function (err, data) {
 		if(err)
 		{
@@ -16,4 +18,4 @@ http.createServer(function (req, res) {
 		res.writeHead(200)
 		res.end(data)
 		})
-	}).listen(3000)
+	}).listen(3000);
