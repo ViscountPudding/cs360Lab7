77712cc6377d422c3d18a03f971764203aba57a9
ViscountPudding <stephenmccammon@comcast.net>
2016-03-07 01:34:56 +0000

    whee

--- nodeSeven/routes/index.js
+++ nodeSeven/routes/index.js
@@ -7,6 +7,22 @@ router.get('/', function(req, res, next) {
   res.sendFile('cityfinder.html', { root: 'public' });
 });
 
+router.get('/test1', function(req, res, next) {
+  res.sendFile('test1.html', { root: 'public' });
+});
+
+router.get('/test2', function(req, res, next) {
+  res.sendFile('test2.txt', { root: 'public' });
+});
+
+router.get('/test3', function(req, res, next) {
+  res.sendFile('test3.gif', { root: 'public' });
+});
+
+router.get('/test4', function(req, res, next) {
+  res.sendFile('test4.jpg', { root: 'public' });
+});
+
 router.get('/getcity',function(req,res,next) {
   fs.readFile(__dirname + '/cities.dat.txt', function(err,data) {
 	if(err) throw err;
